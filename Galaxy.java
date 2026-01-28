883379a8f6da5a2d2f10b264f7610a37c99be921
Qi Yang <96786782+qyscode@users.noreply.github.com>
2026-01-28 23:44:42 +0800

    Level-0

---
+++ Galaxy.java
@@ -0,0 +1,9 @@
+public class Galaxy {
+	public static void main(String[] args) {
+		System.out.println("____________________________________________________________");
+ 		System.out.println("Hello! I'm Galaxy");
+		System.out.println("What can I do for you?");
+		System.out.println("____________________________________________________________\nBye. Hope to see you again soon!");
+		System.out.println("____________________________________________________________");
+	}
+}
