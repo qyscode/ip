ca4dfb88609c7b34c8eee7f0cd7c73c9715dec48
qyscode <96786782+qyscode@users.noreply.github.com>
2026-01-29 01:01:41 +0800

    Level-2

--- src/main/java/Galaxy.java
+++ src/main/java/Galaxy.java
@@ -6,14 +6,22 @@ public class Galaxy {
  		System.out.println("Hello! I'm Galaxy");
 		System.out.println("What can I do for you?");
 		Scanner scanner = new Scanner(System.in);
+		List<String> arrayList = new ArrayList<>();
 		while (true) {
 			String target = scanner.nextLine(); // Read a line of text input
-			if (!target.equals("bye")) {
-				System.out.println(target);
-			} else {
+
+			if (target.equals("bye")) {
 				System.out.println("____________________________________________________________\nBye. Hope to see you again soon!");
 				System.out.println("____________________________________________________________");
 				break;
+			} else if (target.equals("list")) {
+				for (int i=0; i<arrayList.size(); i++) {
+					System.out.println((i+1) + ". " + arrayList.get(i));
+
+				}
+			} else {
+				arrayList.add(target);
+				System.out.println("added: " + target);
 			}
 
 		}
